fa8da42d838b07e62e42f06fd9eb55dc16324a63
ChrisTimperley <christimperley@googlemail.com>
2018-03-06 10:01:20 -0500

    added /status and /adapt endpoints

--- orchestrator/src/orchestrator/server.py
+++ orchestrator/src/orchestrator/server.py
@@ -10,6 +10,18 @@ def list_perturbations():
     return flask.jsonify(jsn)
 
 
+@app.route('/status', methods=['GET'])
+def observe_status():
+    jsn = []
+    return flask.jsonify(jsn)
+
+
+@app.route('/adapt', methods=['POST'])
+def trigger_adaptation():
+    jsn = []
+    return flask.jsonify(jsn)
+
+
 def run(port: int = 8000) -> None:
     app.run(port=port)
 
