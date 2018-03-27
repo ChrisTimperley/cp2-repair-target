06a56bf903f62d0e176ad062528ba8490699e591
ChrisTimperley <christimperley@googlemail.com>
2018-03-27 00:25:44 -0400

    fixed entrypoint script: horrible hack to avoid  interaction

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd "${ROS_WS}"
-# . "/opt/ros/${ROS_DISTRO}/setup.bash"
-# . "${ROS_WS}/devel/setup.bash"
-exec "$@"
+__argv=( "$@" )
+set -- ""
+. "/opt/ros/${ROS_DISTRO}/setup.bash"
+. "${ROS_WS}/devel/setup.bash"
+exec "${__argv}"
